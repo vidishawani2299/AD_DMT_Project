f5be62b88bd2756e05cabc93929885c73e2f9c1e
aadyamishra <aadya.mishra@yale.edu>
2024-04-27 21:42:02 -0400

    params done: add annual costs and dis-utility later

--- Things To Do.py
+++ Things To Do.py
@@ -1,2 +1,3 @@
 # simulate cohort and multicohort for with dmt
 # compare histograms to check for the last bar with the dmt
+# Pass the second cohort - for comparison - read the other cost & utility paper
